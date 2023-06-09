aa2827639a41865b27a34b2d358f8518a12c9831
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2023-06-09 13:42:45 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,26 @@ function App() {
   const [day, setDay] = useState("--")
   const [month, setMonth] = useState("--")
   const [year, setYear] = useState("--")
-  {/* <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>  */}
+  const [dayError, setDayError] = useState("Not filled")
+  const [monthError, setMonthError] = useState("Not filled")
+  const [yearError, setYearError] = useState("Not filled")
   return (
     <div className="main">
       <div className="top">
         <div className="day">
           <label htmlFor="day">DAY</label>
-          <input placeholder="DD" type="number" name="day" min="1" max="31"/>
+          <input autoComplete="off" placeholder="DD" type="number" name="day" min="1" max="31"/>
+          <p className="error">{dayError}</p>
         </div>
         <div className="month">
           <label htmlFor="month">MONTH</label>
-          <input placeholder="MM" type="number" name="month" min="1" max="12"/>
+          <input autoComplete="off" placeholder="MM" type="number" name="month" min="1" max="12"/>
+          <p className="error">{monthError}</p>
         </div>
         <div className="year">
           <label htmlFor="year">YEAR</label>
-          <input placeholder="YYYY" type="number" name="year" min="1" max="2023"/>          
+          <input autoComplete="off" placeholder="YYYY" type="number" name="year" min="1" max="2023"/>  
+          <p className="error">{yearError}</p>        
         </div>
       </div>
       <div className="middle">
